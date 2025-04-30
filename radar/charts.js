225b311707e06a14ca3ae21abe4fe45674d9ac88
ptcee <182003967+ptcee@users.noreply.github.com>
2025-04-30 15:37:21 -0500

    Add files via upload

--- radar/charts.js
+++ radar/charts.js
@@ -1,21 +1,27 @@
 Chart.defaults.backgroundColor = '#c0c0c0';
-Chart.defaults.borderColor = '#c0c0c0';
+Chart.defaults.borderColor = 'rgb(117, 117, 117)';
 Chart.defaults.color = '#fff';
-Chart.defaults.font.size = 18;
+Chart.defaults.font.size = 16;
 Chart.defaults.font.family = "'Palanquin', serif";
 Chart.defaults.elements.point.pointStyle = 'rect';
 Chart.defaults.plugins.legend.position = 'bottom';
+Chart.defaults.plugins.legend.labels.boxWidth = 15;
+Chart.defaults.plugins.legend.labels.boxHeight = 15;
+Chart.defaults.plugins.legend.labels.padding = 15;
+Chart.defaults.plugins.legend.labels.textAlign = 'center';
+
+
 
 let ctx = document.getElementById('charts');
 
 const data = {
     labels: [
-        'DAMAGE',
-        'DURABILITY',
-        'SPEED',
-        'RANGE',
-        'CONTROL',
-        'SUPPORT',
+        'Damage',
+        'Durability',
+        'Speed',
+        'Range',
+        'Control',
+        'Support',
     ],
     datasets: [
         {
@@ -70,10 +76,88 @@ const data = {
             pointHoverBorderColor: 'rgb(223, 201, 1)'
         },
 
+        {
+            label: 'Angel (3)',
+            hidden: true,
+            data: [2, 2, 4, 3, 3, 1],
+            fill: true,
+            backgroundColor: 'rgba(35, 117, 123, 0.2)',
+            borderColor: 'rgb(0, 228, 245)',
+            pointBackgroundColor: 'rgb(0, 228, 245)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(0, 228, 245)'
+        },
+
+        {
+            label: 'Angela (5)',
+            hidden: true,
+            data: [4, 3, 4, 3, 1, 1],
+            fill: true,
+            backgroundColor: 'rgba(98, 70, 14, 0.2)',
+            borderColor: 'rgb(209, 173, 102)',
+            pointBackgroundColor: 'rgb(209, 173, 102)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(209, 173, 102)'
+        },
+
+        {
+            label: 'Ant-Man (3)',
+            hidden: true,
+            data: [3, 2, 1, 1, 1, 1],
+            fill: true,
+            backgroundColor: 'rgba(235, 74, 0, 0.2)',
+            borderColor: 'rgb(235, 74, 0)',
+            pointBackgroundColor: 'rgb(235, 74, 0)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(235, 74, 0)'
+        },
+
+        {
+            label: 'Apocalypse (6)',
+            hidden: true,
+            data: [4, 5, 3, 3, 1, 4],
+            fill: true,
+            backgroundColor: 'rgba(44, 76, 144, 0.2)',
+            borderColor: 'rgb(96, 131, 205)',
+            pointBackgroundColor: 'rgb(96, 131, 205)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(96, 131, 205)'
+        },
+
+        {
+            label: 'Archangel (4)',
+            hidden: true,
+            data: [4, 2, 4, 3, 1, 1],
+            fill: true,
+            backgroundColor: 'rgba(32, 114, 162, 0.2)',
+            borderColor: 'rgb(101, 183, 230)',
+            pointBackgroundColor: 'rgb(101, 183, 230)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(101, 183, 230)'
+        },
+
+        {
+            label: 'Arnim Zola (3)',
+            hidden: true,
+            data: [3, 2, 1, 4, 3, 3],
+            fill: true,
+            backgroundColor: 'rgba(90, 16, 132, 0.2)',
+            borderColor: 'rgb(172, 57, 239)',
+            pointBackgroundColor: 'rgb(172, 57, 239)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(172, 57, 239)'
+        },
+
     ]
 };
 
-new Chart(ctx, {
+var myChart = new Chart(ctx, {
     type: 'radar',
     data: data,
     options: {
@@ -81,11 +165,12 @@ new Chart(ctx, {
             r: {
                 pointLabels: {
                     font: {
-                        size: 14
+                        size: 12
                     },
                 },
                 angleLines: {
-                    display: false
+                    display: true,
+                    color: '#fff',
                 },
                 suggestedMin: 0,
                 suggestedMax: 5,
@@ -93,47 +178,17 @@ new Chart(ctx, {
                 max: 5,
                 ticks: {
                     stepSize: 1,
-                    color: 'rgb(255, 255, 255)',
-                    backdropColor: 'rgba(155, 0, 0 ,0)',
+                    color: 'rgb(199, 199, 199)',
+                    backdropColor: '#363636',
+                    font: {
+                        size: 12
+                    },
                 }
             }
         },
         plugins: {
             legend: {
-                onClick: (click, legendItem, legend) => {
-                    const datasets = legend.legendItems.map((dataset, index) => {
-                        return dataset.text;
-                    });
-                    const index = datasets.indexOf(legendItem.text);
-
-                    if (legend.chart.isDatasetVisible(index) === true) {
-                        legend.chart.hide(index);
-                    } else {
-                        legend.chart.show(index);
-                    }
-
-                },
-                labels: {
-                    generateLabels: (Chart) => {
-                        let visibility = [];
-                        for (let i = 0; i < Chart.data.datasets.length; i++) {
-                            if (Chart.isDatasetVisible(i) === true) {
-                                visibility.push('rgba(255, 255, 255, 1)');
-                            } else {
-                                visibility.push('rgba(255, 255, 255, 0.5)');
-                            }
-                        }
-
-                        return Chart.data.datasets.map(
-                            (dataset, index) => ({
-                                text: dataset.label,
-                                fillStyle: dataset.backgroundColor,
-                                strokeStyle: dataset.borderColor,
-                                fontColor: visibility[index],
-                            })
-                        )
-                    }
-                }
+                display: false,
             }
         },
         elements: {
@@ -142,7 +197,41 @@ new Chart(ctx, {
             }
         }
     },
+
 });
 
+// CHARACTER LEGEND BUTTONS //
+document.getElementById('abom').style.backgroundColor = data.datasets[0].pointBackgroundColor;
+document.getElementById('abom').innerText = data.datasets[0].label;
+
+document.getElementById('av').style.backgroundColor = data.datasets[1].pointBackgroundColor;
+document.getElementById('av').innerText = data.datasets[1].label;
+
+document.getElementById('asm').style.backgroundColor = data.datasets[2].pointBackgroundColor;
+document.getElementById('asm').innerText = data.datasets[2].label;
+
+document.getElementById('a1').style.backgroundColor = data.datasets[3].pointBackgroundColor;
+document.getElementById('a1').innerText = data.datasets[3].label;
+
+document.getElementById('angl').style.backgroundColor = data.datasets[4].pointBackgroundColor;
+document.getElementById('angl').innerText = data.datasets[4].label;
+
+document.getElementById('angla').style.backgroundColor = data.datasets[5].pointBackgroundColor;
+document.getElementById('angla').innerText = data.datasets[5].label;
+
+document.getElementById('ant').style.backgroundColor = data.datasets[6].pointBackgroundColor;
+document.getElementById('ant').innerText = data.datasets[6].label;
 
 
+
+
+function toggleData(value){
+    const visData = myChart.isDatasetVisible(value);
+    if(visData === true ){
+        myChart.hide(value);
+    }
+
+    if(visData === false ){
+        myChart.show(value);
+    }
+}
