db58a41f4c9d7f2c93f9cd2f9d1009a235b5b52e
ptcee <182003967+ptcee@users.noreply.github.com>
2025-04-30 11:26:02 -0500

    Add files via upload

--- radar/charts.js
+++ radar/charts.js
@@ -4,8 +4,9 @@ Chart.defaults.color = '#fff';
 Chart.defaults.font.size = 16;
 Chart.defaults.font.family = "'Palanquin', serif";
 Chart.defaults.elements.point.pointStyle = 'rect';
+Chart.defaults.plugins.legend.label
 
-let ctx = document.getElementById('abom');
+let ctx = document.getElementById('charts');
 
 const data = {
     labels: [
@@ -18,29 +19,29 @@ const data = {
     ],
     datasets: [
         {
-        label: 'Abomination (5)',
-        hidden: true,
-        data: [4, 4, 4, 2, 3, 1],
-        fill: true,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgb(255, 99, 132)',
-        pointBackgroundColor: 'rgb(255, 99, 132)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgb(255, 99, 132)'
-    },
-    {
-        label: 'Agent Venom (4)',
-        hidden: true,
-        data: [4, 3, 3, 4, 3, 1],
-        fill: true,
-        backgroundColor: 'rgba(20, 0, 133, 0.2)',
-        borderColor: 'rgb(147, 128, 255)',
-        pointBackgroundColor: 'rgb(147, 128, 255)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgb(147, 128, 255)'
-    }
+            label: 'Abomination (5)',
+            hidden: true,
+            data: [4, 4, 4, 2, 3, 1],
+            fill: true,
+            backgroundColor: 'rgba(0, 102, 24, 0.2)',
+            borderColor: 'rgb(50, 226, 91)',
+            pointBackgroundColor: 'rgb(50, 226, 91)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(50, 226, 91)'
+        },
+        {
+            label: 'Agent Venom (4)',
+            hidden: true,
+            data: [4, 3, 3, 4, 3, 1],
+            fill: true,
+            backgroundColor: 'rgba(20, 0, 133, 0.2)',
+            borderColor: 'rgb(147, 128, 255)',
+            pointBackgroundColor: 'rgb(147, 128, 255)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(147, 128, 255)'
+        }
 
     ]
 };
@@ -51,22 +52,57 @@ new Chart(ctx, {
     options: {
         scales: {
             r: {
-              angleLines: {
-                display: false
-              },
-              suggestedMin: 0,
-              suggestedMax: 5,
-              min: 0,
-              max: 5,
-              ticks: {
-                stepSize: 1,
-                color: 'rgb(255, 255, 255)',
-                backdropColor: 'rgba(155, 0, 0 ,0)',
-              }
+                angleLines: {
+                    display: false
+                },
+                suggestedMin: 0,
+                suggestedMax: 5,
+                min: 0,
+                max: 5,
+                ticks: {
+                    stepSize: 1,
+                    color: 'rgb(255, 255, 255)',
+                    backdropColor: 'rgba(155, 0, 0 ,0)',
+                }
             }
-          },
+        },
         plugins: {
+            legend: {
+                onClick: (click, legendItem, legend) => {
+                    const datasets = legend.legendItems.map((dataset, index) => {
+                        return dataset.text;
+                    });
+                    const index = datasets.indexOf(legendItem.text);
 
+                    if (legend.chart.isDatasetVisible(index) === true) {
+                        legend.chart.hide(index);
+                    } else {
+                        legend.chart.show(index);
+                    }
+
+                },
+                labels: {
+                    generateLabels: (Chart) => {
+                        let visibility = [];
+                        for (let i = 0; i < Chart.data.datasets.length; i++) {
+                            if (Chart.isDatasetVisible(i) === true) {
+                                visibility.push('rgba(255, 255, 255, 1)');
+                            } else {
+                                visibility.push('rgba(255, 255, 255, 0.5)');
+                            }
+                        }
+
+                        return Chart.data.datasets.map(
+                            (dataset, index) => ({
+                                text: dataset.label,
+                                fillStyle: dataset.backgroundColor,
+                                strokeStyle: dataset.borderColor,
+                                fontColor: visibility[index],
+                            })
+                        )
+                    }
+                }
+            }
         },
         elements: {
             line: {
